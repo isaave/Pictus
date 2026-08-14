23dc292ee6e56a30494acdc9268c3c8dc9510220
isaave <isabellaavelina84626@gmail.com>
2026-08-14 15:37:15 -0300

    Initial Commit

---
+++ Pictus/PictusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictusApp.swift
+//  Pictus
+//
+//  Created by Isabella Avelina on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PictusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
