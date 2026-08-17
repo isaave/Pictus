88befd62ac80f1acbeb993c386c9abfe0ce6da59
Pedro Teruel <pedrohteruel@gmail.com>
2026-08-17 17:10:22 -0300

    Criação do Componente ArtPreview

---
+++ Pictus/Component/ArtPreview.swift
@@ -0,0 +1,18 @@
+//
+//  ArtPreview.swift
+//  Pictus
+//
+//  Created by Pedro Henrique Hossaka Teruel on 17/08/26.
+//
+
+import SwiftUI
+
+struct ArtPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtPreview()
+}
