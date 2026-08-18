a46adb2343d9f4ef693ed9f8546402d25284ce55
monge <pedrokamonge@gmail.com>
2026-08-18 16:56:52 -0300

    banco sem erro subindo

--- database/CoreDataRelationshipViewModel.swift
+++ database/CoreDataRelationshipViewModel.swift
@@ -15,27 +15,14 @@ import SwiftData
 class CoreDataRelationshipViewModel: ObservableObject {
     let manager = CoreDataManage.instance
         
-    func addObra(name: String, nameAutor: String?, dataCriacao: Date, context: String, img: Data, origen: String ){
+    func addObra(name: String?, nameAutor: String?, dataCriacao: Date?, context: String, img: Data, origen: String ){
         
-        guard !name.trimmingCharacters(in: .whitespaces).isEmpty else {
-               print("Erro: nome da obra está vazio")
-               return
-           }
-
-        
-           guard let nameAutor, !nameAutor.trimmingCharacters(in: .whitespaces).isEmpty else {
-               print("Erro: nome do autor está vazio ou nil")
-               return
-           }
-
-       
-        //so vai chegar aqui se nn der nenhum erro
         let newObra = ObraEntity(context: manager.context)
         newObra.ctxObra = context.trimmingCharacters(in: .whitespaces).isEmpty ? nil : context
         
         
         newObra.id = UUID()
-        newObra.nameObra = name
+        newObra.nameObra = context.trimmingCharacters(in: .whitespaces).isEmpty ? "Desconhesido" : name
         newObra.dateObra = dataCriacao
         newObra.imgObra = img
         newObra.origen = origen
@@ -54,6 +41,7 @@ class CoreDataRelationshipViewModel: ObservableObject {
         if let result = try? manager.context.fetch(busca).first {
             result.ctxLibetado = true
         }
+        saveData()
     }
     
     func addReflection(rfx : String){
@@ -61,16 +49,12 @@ class CoreDataRelationshipViewModel: ObservableObject {
        
         newReflexao.textReflx = rfx
         newReflexao.dateReflx = Date()
+        saveData()
     }
  
-    
-    func addAtor(name: String){
-
-    let newAutor = AutorEntity(context: manager.context)
-    newAutor.name = name
-    saveData()
+    func addAlbuns(){
+        
     }
-    
     func saveData() {
     manager.save()
     }
