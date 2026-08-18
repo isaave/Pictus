2dae97a8352e05e4dc75134ac602d90b2e2460f3
André Holovati <andreholovati@gmail.com>
2026-08-18 17:49:31 -0300

    feat(MockWork) Criacao da Classe Obra

--- Pictus/Albuns/WorkOfDay/MockWork.swift
+++ Pictus/Albuns/WorkOfDay/MockWork.swift
@@ -5,8 +5,14 @@
 //  Created by Andre on 17/08/26.
 //
 
-class obras{
-    let obra = {
-        
-    }
+import Foundation
+import SwiftUI
+
+class Obras{
+    let name: String = ""
+    let nameAutor: String = ""
+    let dataCriacao: Date = Date()
+    let context: String = ""
+    let img: Data = Data()
+    let origem: String = ""
 }
