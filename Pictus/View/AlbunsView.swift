6202bce98da95184bd9badf94a85ca2013076a7a
André Holovati <andreholovati@gmail.com>
2026-08-20 17:46:35 -0300

    Criação da função de adicionar obra funcional, edição na toolbar e etc

--- Pictus/View/AlbunsView.swift
+++ Pictus/View/AlbunsView.swift
@@ -17,39 +17,6 @@ struct AlbunsView: View {
     var body: some View {
         NavigationStack {
             VStack(spacing: 0) {
-                // Barra Superior Personalizada (Alinhada como na sua imagem)
-                HStack {
-                    // Botão de Voltar personalizado
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(systemName: "chevron.left")
-                            .font(.system(size: 20, weight: .semibold))
-                            .foregroundColor(.primary)
-                            .frame(width: 48, height: 48)
-                            .glassEffect(.regular.interactive(), in: .rect(cornerRadius: 55, style: .continuous))
-                    }
-                    .shadow(color: .black.opacity(0.08), radius: 10, x: 0, y: 5)
-                    .buttonStyle(.plain)
-                    
-                    Spacer()
-                    
-                    // Título Centralizado
-                    Text("Álbuns")
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundColor(.primary)
-                    
-                    Spacer()
-                    
-                    // Botão de Adicionar (BtnAdd)
-                    BtnAdd(ButtonAction: {
-                        print("Add clicado")
-                    }, icon: "plus")
-                    .frame(width: 48, height: 48)
-                }
-                .padding(.horizontal, 16)
-                .padding(.vertical, 8)
-                
                 // Conteúdo com Scroll
                 ScrollView {
                     LazyVGrid(columns: columns, spacing: 16) {
@@ -61,7 +28,16 @@ struct AlbunsView: View {
                 }
                 .searchable(text: $searchText, prompt: "Buscar álbuns")
             }
-            .navigationBarHidden(true) // Oculta a barra nativa para usar apenas a nossa personalizada
+          
+        }
+        .navigationTitle("Albuns")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button{
+                }label: {
+                    Image(systemName: "plus")
+                }
+            }
         }
     }
 }
